e8ae39d23ff384ab653d1fc572d80594e30d4981
alysvolatile <alice.g.cooper@gmail.com>
2022-10-31 11:50:09 -0400

    set up for responsive menu v2

--- script.js
+++ script.js
@@ -1,49 +1,34 @@
 const links = document.querySelectorAll(".nav-list li a");
 
-for (let link of links) {
-    link.addEventListener('click', smoothScroll)
+for (link of links) {
+  link.addEventListener("click", smoothScroll);
 }
 
 function smoothScroll(e) {
-    e.preventDefault();
-    const href = this.getAttribute("href");
-    document.querySelector(href).scrollIntoView({
-        behavior: "smooth",
-    });
+  e.preventDefault();
+  const href = this.getAttribute("href");
+  document.querySelector(href).scrollIntoView({
+    behavior: "smooth",
+  });
+  hideMenu();
 }
 
-window.addEventListener('scroll', () => {
-    const header = document.querySelector('header');
-    header.classList.toggle('sticky', window.scrollY > 0)
-})
+// RESPONSIVE MOBILE MENU
+const menu = document.querySelector(".nav-list");
+const hamburger = document.querySelector(".hamburger");
+const close = document.querySelector(".close");
 
-const navList = document.querySelector(".nav-list")
-navList.addEventListener("click", (e) => {
-    const navLink = e.target.parentElement
-    if (navLink.classList.contains("link")) {
-        navList.querySelector(".active").classList.remove("active");
-        navLink.classList.add("active")
-    }
-})
+const showMenu = () => {
+  hamburger.style.display = "none";
+  close.style.transform = "translateY(0)";
+  menu.style.transform = "translateY(0)";
+};
 
-const scrollButton = document.querySelector(".top")
-const rootElement = document.documentElement;
+const hideMenu = () => {
+  close.style.transform = "translateY(-20rem)";
+  hamburger.style.display = "block";
+  menu.style.transform = "translateY(-200%)";
+};
 
-document.addEventListener("scroll", showButton)
-scrollButton.addEventListener("click", scrollToTop)
-
-function showButton() {
-    const scrollTotal = rootElement.scrollHeight - rootElement.clientHeight
-    if (rootElement.scrollTop / scrollTotal > 0.3) {
-        scrollButton.classList.add('show-btn')
-    } else {
-        scrollButton.classList.remove('show-btn')
-    }
-}
-
-function scrollToTop() {
-    rootElement.scrollTo({
-        top: 0,
-        behavior: "smooth"
-    })
-}
+hamburger.addEventListener("click", showMenu);
+close.addEventListener("click", hideMenu);
